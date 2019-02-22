72b8e1c8c2f119844064552f327986063d031806
Robert Junio <rjmonteiro@indracompany.com>
2019-02-22 13:42:10 -0300

    Aula 47 - Serviço http para detalhes do restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -11,31 +11,17 @@ import { ErrorHandler } from '../app.error-handler'
 @Injectable()
 export class RestaurantsService {
 
-
-//   rests: Restaurant[] = [
-//   {
-//     id: "bread-bakery",
-//     name: "Bread & Bakery",
-//     category: "Bakery",
-//     deliveryEstimate: "25m",
-//     rating: 4.9,
-//     imagePath: "assets/img/restaurants/breadbakery.png"
-//   },
-//   {
-//     id: "burger-house",
-//     name: "Burger House",
-//     category: "Hamburgers",
-//     deliveryEstimate: "100m",
-//     rating: 3.5,
-//     imagePath: "assets/img/restaurants/burgerhouse.png"
-//   }
-// ]
-
   constructor(private http: Http){}
 
   restaurants(): Observable<Restaurant[]> {
     // map necessário para mapear a resposta, obtendo somente o json que vem com a resposta
     return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
-    .catch(ErrorHandler.handleError)
+    .catch(ErrorHandler.handleError);
   }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`).map(response => response.json())
+    .catch(ErrorHandler.handleError);
+  }
+  
 }

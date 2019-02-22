1467ebd6f2369333feaf5e383b2a74eed77e2615
Robert Junio <rjmonteiro@indracompany.com>
2019-02-22 14:12:37 -0300

    Aula 48 - Adicionando rotas filhas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import {HomeComponent} from './home/home.component'
 import {AboutComponent} from './about/about.component'
 import {RestaurantsComponent} from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
-
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { MenuComponent } from './restaurant-detail/menu/menu.component'
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'restaurants', component: RestaurantsComponent},
-  {path: 'restaurants/:id', component: RestaurantDetailComponent}
+  {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      {path: '', redirectTo: 'menu', pathMatch: 'full'}, // redicerionar pára menu ao entrar no compoennt na primeira vez por nao ter nada
+      {path: 'menu', component: MenuComponent},
+      {path: 'reviews', component: ReviewsComponent},
+    ]}
 ]

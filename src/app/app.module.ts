45966c78082e88d6786d3978eabbe73b604cddf9
Robert Junio <rjmonteiro@indracompany.com>
2019-05-13 15:19:02 -0300

    Aula 65 - Implementando componente de items da compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component'
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component'
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { RadioComponent } from './shared/radio/radio.component'
     OrderComponent,
     InputComponent,
     RadioComponent,
+    OrderItemsComponent,
 
   ],
   imports: [

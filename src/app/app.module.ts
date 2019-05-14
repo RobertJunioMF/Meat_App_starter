78ef95543fb8d3ea89991c3f85482d931083a64f
Robert Junio <rjmonteiro@indracompany.com>
2019-05-14 13:51:30 -0300

    Aula 67 - Criando componente total de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { OrderService } from './order/order.service';
     InputComponent,
     RadioComponent,
     OrderItemsComponent,
+    DeliveryCostsComponent,
 
   ],
   imports: [

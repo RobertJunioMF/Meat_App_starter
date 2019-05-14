16574feaf57fce12cf4d78526fe608c6a9a65acd
Robert Junio <rjmonteiro@indracompany.com>
2019-05-14 16:04:09 -0300

    Aula 68 - Finalizando a compra (HTTP POST)

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+import { MEAT_API } from '../app.api';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService){}
+  constructor(private cartService: ShoppingCartService,
+              private http: Http){}
 
   itemsValue(): number {
     return this.cartService.total();
@@ -25,6 +31,17 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+          .map(response => response.json());
+  }
+
+  clear() {
+    this.cartService.clear();
   }
 
 }

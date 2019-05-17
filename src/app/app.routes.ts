edb51ef2f61815d00d55e35ce102933f9aa47d55
Robert Junio <rjmonteiro@indracompany.com>
2019-05-17 15:20:37 -0300

    Aula 76 - Lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
-import { AboutComponent } from './about/about.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
@@ -10,7 +9,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component'
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
   {path: 'restaurants', component: RestaurantsComponent},
   {path: 'order', component: OrderComponent},
   {path: 'order-summary', component: OrderSummaryComponent},

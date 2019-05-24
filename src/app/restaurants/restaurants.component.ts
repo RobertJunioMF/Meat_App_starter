a086ad4484825bea63c5e836316e5be6c8aceafa
Robert Junio <rjmonteiro@indracompany.com>
2019-05-24 16:33:58 -0300

    Aula 103 - Aplicando animações na barra de busca

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,40 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantsService} from './restaurants.service'
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0, "max-lenght": "0px"
+      })),
+      state('visible', style({
+        opacity: 1, "max-lenght": "70px", "margin-top": "20px", "margin-bottom": "40px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[]
-/*
-  // array ainda utilizado pela falta do backend
-  restaurants: Restaurant[] = [
-  {
-    id: "bread-bakery",
-    name: "Bread & Bakery",
-    category: "Bakery",
-    deliveryEstimate: "25m",
-    rating: 4.9,
-    imagePath: "assets/img/restaurants/breadbakery.png"
-  },
-  {
-    id: "burger-house",
-    name: "Burger House",
-    category: "Hamburgers",
-    deliveryEstimate: "100m",
-    rating: 3.5,
-    imagePath: "assets/img/restaurants/burgerhouse.png"
-  }
-] */
+  searchBarState = 'hidden'
 
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
     // metodo subscribe para receber o map enviado pelo serviço
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }

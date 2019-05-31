bacec875280d359ac6d4f9a53c76a4b04e1c8646
Robert Junio <rjmonteiro@indracompany.com>
2019-05-31 15:10:59 -0300

    Aula 122 - Disparando login automaticamente

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -3,16 +3,21 @@ import { HttpClient } from '@angular/common/http'
 import { MEAT_API } from '../../app.api'
 import { User } from './user.model'
 import { Observable } from 'rxjs/Observable'
-import { Router } from '@angular/router'
+import { Router, NavigationEnd } from '@angular/router'
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
 
 @Injectable()
 export class LoginService {
 
   user: User
+  lastUrl: string
 
   constructor(private http: HttpClient,
-              private router: Router){}
+              private router: Router){
+        this.router.events.filter(e => e instanceof NavigationEnd)
+                          .subscribe((e: NavigationEnd) => this.lastUrl = e.url)
+  }
 
   login(email: string, password: string): Observable<any> {
     return this.http.post<User>(`${MEAT_API}/login`, {email: email, password: password})
@@ -23,7 +28,11 @@ export class LoginService {
     return this.user !== undefined
   }
 
-  handleLogin(path?: string) {
+  handleLogin(path: string = this.lastUrl) {
     this.router.navigate(['/login', btoa(path)])
+  }
+
+  logout() {
+    this.user = undefined
   }
 }

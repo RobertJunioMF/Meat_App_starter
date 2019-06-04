e1a2b93289d76d0eff1ca97bf7b97d46b5148969
Robert Junio <rjmonteiro@indracompany.com>
2019-06-04 15:34:26 -0300

    Aula 127 - Utilizando Rotas
    Finalizando curso, porém não foi possivel atualizapara versao 6 e 7 do angular.

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Injector } from '@angular/core'
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core'
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
 import { NotificationService } from './shared/messages/notifications.service'
@@ -9,24 +9,27 @@ import 'rxjs/add/observable/throw'
 export class ApplicatonErrorHandler extends ErrorHandler {
 
   constructor(private ns: NotificationService,
-              private injector: Injector) {
+              private injector: Injector,
+              private zone: NgZone) {
     super()
   }
 
   handleError(errorResponse: HttpErrorResponse | any){
     if(errorResponse instanceof HttpErrorResponse) {
       const message = errorResponse.error.message
-      switch(errorResponse.status) {
-        case 401:
-          this.injector.get(LoginService).handleLogin()
-          break;
-        case 403:
-          this.ns.notify(message || 'Não autorizado.')
-          break;
-        case 404:
-          this.ns.notify(message || 'Recurso não encontrado.')
-          break;
-      }
+      this.zone.run(() => {
+        switch(errorResponse.status) {
+          case 401:
+            this.injector.get(LoginService).handleLogin()
+            break;
+          case 403:
+            this.ns.notify(message || 'Não autorizado.')
+            break;
+          case 404:
+            this.ns.notify(message || 'Recurso não encontrado.')
+            break;
+        }
+      })
     }
     super.handleError(errorResponse)
   }
